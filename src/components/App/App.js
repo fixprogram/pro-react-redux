0e6eb9297f25cf4f095e418b4053483e39d00748
fixprogram <fixprogram@mail.ru>
2020-11-11 09:56:23 +0300

    Basics of React

---
+++ src/components/App/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import AppHeader from "../AppHeader";
+import SearchPanel from "../SearchPanel";
+import TodoList from "../TodoList";
+import ItemStatusFilter from "../ItemStatusFilter";
+
+import "./App.css";
+
+const App = () => {
+  const todoData = [
+    {
+      id: 1,
+      label: "Drink Coffee",
+      important: false,
+    },
+    {
+      id: 2,
+      label: "Make awesome app",
+      important: true,
+    },
+    {
+      id: 3,
+      label: "Have a lunch",
+      important: false,
+    },
+  ];
+
+  return (
+    <div class="App">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
+    </div>
+  );
+};
+
+export default App;
